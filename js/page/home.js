56680f6bd4a6bae067c4b235d5c6e451a7996f9f
yzycake <916363040@qq.com>
2018-03-22 21:36:56 +0800

    Update home.js

--- js/page/home.js
+++ js/page/home.js
@@ -4,7 +4,7 @@ $(function  () {
 		var i=1;
 		setInterval(function  () {
 			i=i++<3?i:1;
-			$("#homeImgWall").css("background","url(/magic/img/home_img_wall/img_wall_"+i+".jpg) 0px -120px no-repeat");
+			$("#homeImgWall").css("background","url(/img/home_img_wall/img_wall_"+i+".jpg) 0px -120px no-repeat");
 			$("#homeImgWall").css("background-size","cover");
 		},3000)
 	}
